4eeeedcf795259f256fb5f7d3f74fcf74385ed48
Dzmitry Zakhar <dzmitry_zakhar@epam.com>
2019-11-25 18:23:48 +0300

    feat: add remembering

--- webapp/controller/MainPage.controller.js
+++ webapp/controller/MainPage.controller.js
@@ -17,6 +17,7 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("eng.English.controller.MainPage", {
+
 		onInit: function () {
 			this.oViewModel = models.createViewModel();
 			this.getView().setModel(this.oViewModel, "oViewModel");
@@ -24,7 +25,29 @@ sap.ui.define([
 
 		onBeforeRendering: function () {
 			window.th = this;
-			this.oViewModel.setProperty("/ListItems", data);
+			this.oViewModel.setProperty("/ListItems", this._updateData());
+		},
+		
+		_updateData: function(){
+			if (window.localStorage.getItem("aToRevise")) {
+				var aToRevise = JSON.parse(window.localStorage.getItem("aToRevise"));
+			} else {
+				return data;
+			}
+			var now = new Date().getTime();
+			var aDataToDisplay = [...data];
+
+			if (Array.isArray(aToRevise)) {
+			
+				aToRevise.forEach((value, index) => {
+				
+					if (value && now < value.nextReviseDate) {
+					
+						aDataToDisplay.splice(index, 1);
+					}
+				});
+			}
+			return aDataToDisplay;
 		},
 
 		onPlay: function (oEvent) {
@@ -162,35 +185,105 @@ sap.ui.define([
 		},
 
 		onInputLiveChange: function (oEvent, sValue) {
-			var oInput = oEvent.getSource();
-			var iIndex = [...oInput.getParent().getBindingContextPath()].pop();
+			// var oInput = oEvent.getSource();
+			// var iIndex = [...oInput.getParent().getBindingContextPath()].pop();
+			// var sEn = data[iIndex].sEn.toLowerCase();
+			// var sInputValue = sValue;
+			// if (sInputValue) {
+			// 	sInputValue = sInputValue.toLowerCase();
+			// } else {
+			// 	sInputValue = "";
+			// }
+			// if (sEn === sInputValue) {
+			// 	oInput.setValueState("Success");
+			// 	oInput.setEditable(false);
+			// 	// some Callback Function should be here
+			// 	this._setNextRevise(oInput);
+			// }
+		},
+
+		_setNextRevise: function (oInput, bReNew, iIndex) {
+			var aToRevise = [];
+			var iNumberOfWord = +[...oInput.getParent().getBindingContextPath()].pop();
+
+			if (!window.localStorage.getItem("aToRevise")) {
+				window.localStorage.setItem("aToRevise", JSON.stringify(aToRevise));
+			}
+			aToRevise = JSON.parse(window.localStorage.getItem("aToRevise"));
+			// check if it alredy has property to revise
+			if (aToRevise[iNumberOfWord] && !bReNew) {
+				aToRevise[iNumberOfWord] = this._setNewDateOfRevise(aToRevise[iNumberOfWord]);
+			} else {
+				aToRevise[iNumberOfWord] = {
+					nextReviseDate: new Date().getTime() + 3600 * 1000 * 24,
+					numberOfRevise: 1,
+				};
+			};
+		
+			window.localStorage.setItem("aToRevise", JSON.stringify(aToRevise))
+		},
+
+		_setNewDateOfRevise: function (iNumberOfRevise) {
+			var oReviseObject = {};
+			var iDay = 3600 * 1000 * 24;
+			switch (iNumberOfRevise) {
+				case 1:
+					oReviseObject = {
+						nextReviseDate: new Date().getTime() + iDay * 1
+					};
+					break;
+				case 2:
+					oReviseObject = {
+						nextReviseDate: new Date().getTime() + iDay * 1
+					};
+					break;
+				case 3:
+					oReviseObject = {
+						nextReviseDate: new Date().getTime() + iDay * 5
+					};
+					break;
+				case 4:
+					oReviseObject = {
+						nextReviseDate: new Date().getTime() + iDay * 23
+					};
+					break;
+			};
+			oReviseObject.numberOfRevise = iNumberOfRevise + 1;
+			return oReviseObject;
+		},
+
+		onSubmitAnswer: function (oEvent) {
+			var oButton = oEvent.getSource();
+			var iIndex = [...oButton.getParent().getBindingContextPath()].pop();
 			var sEn = data[iIndex].sEn.toLowerCase();
-			var sInputValue = sValue;
+			var sInputValue = oButton.getParent().getCells()[5].getValue();
 			if (sInputValue) {
 				sInputValue = sInputValue.toLowerCase();
 			} else {
 				sInputValue = "";
 			}
 			if (sEn === sInputValue) {
-				oInput.setValueState("Success");
-				oInput.setEditable(false);
 				// some Callback Function should be here
-			}
+				this._setNextRevise(oButton);
+			} else {
+				this._setNextRevise(oButton, true);
+			}		
+			this.oViewModel.setProperty("/ListItems", this._updateData());
 		},
 
-		_getColumns: function(sPath){
+		_getColumns: function (sPath) {
 			var aResult = [];
 			var aData = this.oViewModel.getProperty(sPath);
 			if (Array.isArray(aData) && aData.length) {
 				// for (var key in aData[0]) {
-					aResult.push(
-						new ExportColumn({
-							name: "Some",
-							template: new ExportCell({
-								content: "{" + "sRu" + "}"
-							})
-						}),
-					);						
+				aResult.push(
+					new ExportColumn({
+						name: "Some",
+						template: new ExportCell({
+							content: "{" + "sRu" + "}"
+						})
+					}),
+				);
 				// };
 			}
 			return aResult;
@@ -199,20 +292,20 @@ sap.ui.define([
 		onDownload: function () {
 			var sPath = "/ListItems";
 			new Export({
-				models: this.oViewModel,
-				exportType: new ExportTypeCSV({
-					fileExtension: "csv",
-					separatorChar: ";"
-				}),
-				rows: {
-					path: sPath
-				},
-				columns: this._getColumns(sPath)
-			})
-			.saveFile("SuperData")
-			.always(function(){
-				this.destroy();
-			});
+					models: this.oViewModel,
+					exportType: new ExportTypeCSV({
+						fileExtension: "csv",
+						separatorChar: ";"
+					}),
+					rows: {
+						path: sPath
+					},
+					columns: this._getColumns(sPath)
+				})
+				.saveFile("SuperData")
+				.always(function () {
+					this.destroy();
+				});
 		},
 
 		onUpload: function () {
@@ -226,7 +319,7 @@ sap.ui.define([
 			oInput.click();
 			oInput.addEventListener("change", this._onInputChange.bind(this))
 		},
-		
+
 		_onInputChange: function (oEvent) {
 			this.start = new Date();
 			var sResult, aFiles, oFileReader;
@@ -247,8 +340,7 @@ sap.ui.define([
 
 		_handleData: function (sResult) {
 			this.end = new Date();
-			console.log(this.end - this.start + " ms")
-			debugger;
+			console.log(this.end - this.start + " ms");
 		}
 	});
 });
